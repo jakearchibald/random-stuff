7285ea27c568e05d80de260a8e8a8f571f53b7ca
Jake Archibald <jaffathecake@gmail.com>
2025-10-15 10:31:02 -0400

    Add timeout param

--- public/bug-repros/top-level-await-webkit/modules/module-c.js
+++ public/bug-repros/top-level-await-webkit/modules/module-c.js
@@ -1,2 +1,4 @@
-await new Promise((r) => setTimeout(r, 0));
+const searchParams = new URLSearchParams(location.search);
+const timeout = Number(searchParams.get('timeout')) || '0';
+await new Promise((r) => setTimeout(r, timeout));
 export const someConst = 'someConst';
